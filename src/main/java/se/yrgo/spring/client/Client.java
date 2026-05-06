45bbc4260375824d54f6f5dad2564484abfbab40
rutmoaj <rutmoaj6@gmail.com>
2026-05-06 11:42:22 +0200

    trying client class

--- src/main/java/se/yrgo/spring/client/Client.java
+++ src/main/java/se/yrgo/spring/client/Client.java
@@ -1,5 +1,25 @@
 package se.yrgo.spring.client;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import se.yrgo.spring.services.author.AuthorService;
+import se.yrgo.spring.services.book.BookService;
+import se.yrgo.spring.services.user.UserService;
+
 public class Client {
-    
+    public static void main(String[] args) {
+
+        try (ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml")) {
+
+            AuthorService author = container.getBean(AuthorService.class);
+            BookService book = container.getBean(BookService.class);
+            UserService user = container.getBean(UserService.class);
+
+
+        } catch (Exception ex) {
+            System.out.println("Something went wrong: " + ex.getMessage());
+        }
+
+    }
+
 }
--- src/main/java/se/yrgo/spring/dataaccess/BookDaoJPAImpl.java
+++ src/main/java/se/yrgo/spring/dataaccess/BookDaoJPAImpl.java
@@ -2,6 +2,8 @@ package se.yrgo.spring.dataaccess;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
