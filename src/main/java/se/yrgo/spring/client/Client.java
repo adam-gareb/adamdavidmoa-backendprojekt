fbd5a0abee1d0fc9f26d348a027c8d242cfc997b
rutmoaj <rutmoaj6@gmail.com>
2026-05-12 10:54:08 +0200

    working with adminmenu

--- src/main/java/se/yrgo/spring/client/Client.java
+++ src/main/java/se/yrgo/spring/client/Client.java
@@ -3,7 +3,9 @@ package se.yrgo.spring.client;
 public class Client {
     public static void main(String[] args) {
 
-        
+        LibraryApplication app = new LibraryApplication();
+
+        app.runLibrary();
 
     }
 
