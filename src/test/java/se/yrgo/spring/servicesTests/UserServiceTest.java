3b2e5278f2b3e012b767907a1de0ba0f8f73da38
greenwzrd6 <t4kikun@gmail.com>
2026-05-13 12:20:17 +0200

    added junit in pom.xml

--- src/test/java/se/yrgo/spring/servicesTests/UserServiceTest.java
+++ src/test/java/se/yrgo/spring/servicesTests/UserServiceTest.java
@@ -1,5 +1,13 @@
 package se.yrgo.spring.servicesTests;
 
+import org.junit.jupiter.api.extension.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit.jupiter.*;
+import org.springframework.transaction.annotation.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration({ "/other-tiers.xml", "/datasource-test.xml" })
+@Transactional
 public class UserServiceTest {
 
 }
